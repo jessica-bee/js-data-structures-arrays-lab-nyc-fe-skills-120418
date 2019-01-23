d50390280aa38debde3bd6edaa1b828a0a39936b
Jessica Bee <bee.jess@gmail.com>
2019-01-23 00:51:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendDriver(newName) {
+  drivers.push(newName)
+} 
+function destructivelyPrependDriver(newStartName){
+  drivers.unshift(newStartName)
+}
+function destructivelyRemoveLastDriver(removeLastName){
+  drivers.pop()
+}
+function destructivelyRemoveFirstDriver(removeFirstName){
+  drivers.shift()
+}
+function appendDriver (unchanged){
+  drivers.length=0
+}
